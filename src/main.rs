2987930632314362d43298001bd97bd355614e7e
xoy <2088437179@qq.com>
2024-05-06 16:17:50 +0800

    enum practice

--- src/main.rs
+++ src/main.rs
@@ -1805,7 +1805,6 @@
 
 //     let one  =tup.2;
 
-
 // }
 
 // use std::usize;
@@ -1814,7 +1813,6 @@
 //   let s1 = String::from("hello");
 //   // let tup: (String, usize) =  calculate_length(s1);
 //   // println!("{},{}",tup.0,tup.1);
-
 
 //   let (s2, len) = calculate_length(s1);
 
@@ -1825,9 +1823,9 @@
 // fn calculate_length(s:String) -> (String, usize) {
 //   let len = s.len();
 //   (s, len)
-// } 
+// }
 
-// tuple practice 
+// tuple practice
 // 1
 
 // fn main() {
@@ -1873,12 +1871,11 @@
 
 //   // 填空
 //   (y, z, x) = (1, 2, 3);
-  
+
 //   assert_eq!(x, 3);
 //   assert_eq!(y, 1);
 //   assert_eq!(z, 2);
 // }
-
 
 // 6
 // fn main() {
@@ -1893,4 +1890,443 @@
 //   (nums.0 + nums.1, nums.0 * nums.1)
 // }
 
-// 结构体
+// // 结构体
+// struct User {
+//   activate: bool,
+//   username: String,
+//   email: String,
+//   sign_in_count: u64,
+// }
+// fn main() {
+
+// // 创建结构体实例
+// // let user1 = User{
+// //   email: String::from("someone@example.com"),
+// //   username: String::from("someusername123"),
+// //   activate: true,
+// //   sign_in_count: 1,
+// // };
+
+// let mut user1 = User{
+//   email: String::from("someone@example.com"),
+//   username: String::from("someusername123"),
+//   activate: true,
+//   sign_in_count: 1,
+// };
+
+// user1.email=String::from("anotheremail@example.com");
+
+// let user1 = bulid_user("email".to_string(), "username".to_string());
+// }
+
+// // 简化结构体创建
+// fn bulid_user(email: String, username: String) -> User {
+//     // User {
+//     //     email: email,
+//     //     username: username,
+//     //     activate: true,
+//     //     sign_in_count: 1,
+//     // }
+
+//     User {
+//         email,
+//         username,
+//         activate: true,
+//         sign_in_count: 1,
+//     }
+// }
+
+// fn main() {
+//     struct User {
+//         activate: bool,
+//         username: String,
+//         email: String,
+//         sign_in_count: u64,
+//     }
+
+//     let mut user1 = User {
+//         email: String::from("someone@example.com"),
+//         username: String::from("someusername123"),
+//         activate: true,
+//         sign_in_count: 1,
+//     };
+
+//     let user2 = User {
+//         activate: user1.activate,
+//         username: user1.username,
+//         email: String::from("another@example.com"),
+//         sign_in_count: user1.sign_in_count,
+//     };
+
+//     // username 所有权被转移给了 user2，导致了 user1 无法再被使用，
+//     // 但是并不代表 user1 内部的其它字段不能被继续使用
+//     println!("{}",user1.activate);
+
+//     println!("{:?}",user1);
+
+//     // let user2 = User {
+//     //   email: String::from("another@example.com"),
+//     // //  结构体更新语法 ..必须在结构体尾部使用
+//     //   ..user1
+//     // };
+// }
+
+// use std::fs::File;
+
+// 结构体内存排列
+// #[derive(Debug)]
+// struct File {
+//   name: String,
+//   data: Vec<u8>,
+// }
+
+// fn main() {
+//   let f1 = File {
+//     name: String::from("f1.txt"),
+//     data:Vec::new(),
+//   };
+
+//   let f1_name = &f1.name;
+//   let f1_length = &f1.data.len();
+
+//   println!("{:?}", f1);
+//   println!("{} is {} bytes long", f1_name, f1_length);
+
+// }
+
+// 元组结构体
+// struct  Color (i32,i32,i32);
+// struct Point (i32,i32,i32);
+
+// let black = Color(0,0,0);
+// let orange = Point(0,0,0);
+
+// // 单元结构体
+// struct AlwayEqual;
+// fn main() {
+//   let subject = AlwayEqual;
+
+//   impl SomeTrait for AlwayEqual {
+
+//   }
+// }
+
+// 结构体数据的所有权
+// struct User {
+//   username: &str,
+//   email: &str,
+//   sign_in_count: u64,
+//   active: bool,
+// }
+
+// fn main() {
+//   let user1 = User {
+//       email: "someone@example.com",
+//       username: "someusername123",
+//       active: true,
+//       sign_in_count: 1,
+//   };
+// }
+
+// #[derive(Debug)]
+// #[derive(Debug)]
+// struct Rectangle {
+//     width: u32,
+//     height: u32,
+// }
+
+// fn main() {
+//     let scale = 2;
+//     let rect1 = Rectangle {
+//         width: dbg!(30 * scale),
+//         height: 50,
+//     };
+
+//     dbg!(&rect1);
+// }
+
+// prcatice
+
+// 1
+// fix the error
+// struct Person {
+//   name: String,
+//   age: u8,
+//   hobby: String
+// }
+// fn main() {
+//   let age = 30;
+//   let p = Person {
+//       name: String::from("sunface"),
+//       age:11,
+//       hobby:String::from("lanqiu")
+//   };
+// }
+
+// 2
+
+// struct Unit;
+// trait SomeTrait {
+//     // ...定义一些行为
+// }
+
+// // 我们并不关心结构体中有什么数据( 字段 )，但我们关心它的行为。
+// // 因此这里我们使用没有任何字段的单元结构体，然后为它实现一些行为
+// impl SomeTrait for Unit {  }
+// fn main() {
+//     let u = Unit;
+//     do_something_with_unit(u);
+// }
+
+// // 填空，让代码工作
+// fn do_something_with_unit(u: Unit) {   }
+
+// 3
+
+// // // 填空并修复错误
+// struct Color(i32, i32, i32);
+// struct Point(i32, i32, i32);
+// fn main() {
+//     let v = Point(0, 127, 255);
+//     check_color(v);
+// }
+
+// fn check_color(p: Point) {
+//     let Point(x, _, _) = p;
+//     assert_eq!(x, 0);
+//     assert_eq!(p.1, 127);
+//     assert_eq!(p.2, 255);
+//  }
+
+// 4
+
+// 填空并修复错误，不要增加或移除代码行
+// struct Person {
+//   name: String,
+//   age: u8,
+// }
+// fn main() {
+//   let age = 18;
+//   let mut p = Person {
+//       name: String::from("sunface"),
+//       age,
+//   };
+
+//   // how can you believe sunface is only 18?
+//   p.age = 30;
+
+//   // 填空
+//   p.name = String::from("sunfei");
+// }
+
+// 5
+
+// 填空
+// struct Person {
+//   name: String,
+//   age: u8,
+// }
+// fn main() {}
+
+// fn build_person(name: String, age: u8) -> Person {
+//   Person {
+//       age,
+//       name,
+//   }
+// }
+
+// 6
+
+// 填空，让代码工作
+// struct User {
+//     active: bool,
+//     username: String,
+//     email: String,
+//     sign_in_count: u64,
+// }
+// fn main() {
+//     let u1 = User {
+//         email: String::from("someone@example.com"),
+//         username: String::from("sunface"),
+//         active: true,
+//         sign_in_count: 1,
+//     };
+
+//     let u2 = set_email(u1);
+// }
+
+// fn set_email(u: User) -> User {
+//     User {
+//         email: String::from("contact@im.dev"),
+//         ..u
+//     }
+// }
+
+// 7
+
+// 填空，让代码工作
+// #[derive(Debug)]
+// struct Rectangle {
+//     width: u32,
+//     height: u32,
+// }
+
+// fn main() {
+//     let scale = 2;
+//     let rect1 = Rectangle {
+//         width: dbg!(30 * scale), // 打印 debug 信息到标准错误输出 stderr,并将 `30 * scale` 的值赋给 `width`
+//         height: 50,
+//     };
+
+//     dbg!(&rect1); // 打印 debug 信息到标准错误输出 stderr
+
+//     println!("{:?}", rect1); // 打印 debug 信息到标准输出 stdout
+// }
+
+// 8
+
+// fn main() {
+//   #[derive(Debug)]
+//   struct Person {
+//       name: String,
+//       age: Box<u8>,
+//   }
+
+//   let person = Person {
+//       name: String::from("Alice"),
+//       age: Box::new(20),
+//   };
+
+//   // 通过这种解构式模式匹配，person.name 的所有权被转移给新的变量 `name`
+//   // 但是，这里 `age` 变量却是对 person.age 的引用, 这里 ref 的使用相当于: let age = &person.age
+//   let Person { name, ref age } = person;
+
+//   println!("The person's age is {}", age);
+
+//   println!("The person's name is {}", name);
+
+//   // Error! 原因是 person 的一部分已经被转移了所有权，因此我们无法再使用它
+//   //println!("The person struct is {:?}", person);
+
+//   // 虽然 `person` 作为一个整体无法再被使用，但是 `person.age` 依然可以使用
+//   println!("The person's age from person struct is {}", person.age);
+// }
+
+// 9
+
+// 修复错误
+// #[derive(Debug)]
+// struct File {
+//     name: String,
+//     data: String,
+// }
+// fn main() {
+//     let f = File {
+//         name: String::from("readme.md"),
+//         data: "Rust By Practice".to_string()
+//     };
+
+//     let _name = f.name;
+
+//     // 只能修改这一行
+//     println!("{}", f.data);
+//     // println!("{}, {}, {:?}",f.name, f.data, f);
+// }
+
+// 枚举
+// #[derive(Debug)]
+// enum PokerSuit {
+//   Clubs,
+//   Spades,
+//   Diamonds,
+//   Hearts,
+// }
+
+// struct PockerCard {
+//   suit: PokerSuit,
+//   value:u8
+// }
+
+// fn main() {
+//   //枚举值
+//   // let heart = PokerSuit::Hearts;
+//   // let diamond = PokerSuit::Diamonds;
+
+//   // print_suit(heart);
+//   // print_suit(diamond);
+
+//   let c1 = PockerCard {
+//     suit: PokerSuit::Clubs,
+//     value: 1,
+//   };
+
+//   let c2 = PockerCard {
+//     suit: PokerSuit::Diamonds,
+//     value:12,
+//   };
+
+// }
+
+// fn print_suit(card: PokerSuit) {
+
+//   println!("{:?}",card);
+// }
+
+// enum PockerCard {
+//   Clubs(u8),
+//   Spades(u8),
+//   // Diamonds(u8),
+//   // Hearts(u8),
+//   Diamonds(char),
+//   Hearts(char),
+// }
+
+// fn main() {
+//   let c1 = PockerCard::Clubs(1);
+//   let c2 = PockerCard::Diamonds('A');
+// }
+
+// enum Message {
+//   Quit,
+//   Move {x:i32, y: i32}, //匿名结构体
+//   Writer(String),
+//   ChangeColor(i32,i32,i32),
+// }
+
+// fn main() {
+//   let m1 = Message::Quit;
+//   let m2 = Message::Move { x: 1, y: 2 };
+//   let m3 = Message::ChangeColor(255, 255, 0);
+// }
+
+// Option 枚举用于处理空值
+// fn main() {
+//     // let some_number = Some(5);
+//     // let some_string = Some("a string");
+
+//     // let absent_number: Option<i32> = None;
+
+//     // let x: i8 = 5;
+//     // let y: Option<i8> = Some(5);
+
+//     // let sum = x + y;
+
+//     fn plus_one(x: Option<i32>) -> Option<i32> {
+//       match x {
+//           None => None,
+//           Some(i) => Some(i + 1),
+//       }
+//   }
+  
+//   let five = Some(5);
+//   let six = plus_one(five);
+//   let none = plus_one(None);
+
+
+//   dbg!(six);
+//   dbg!(none);
+  
+// }
+
+// enum practice
+
