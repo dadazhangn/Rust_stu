dbcc695a82b13d5ae80e57ca639ecef85292166e
xoy <2088437179@qq.com>
2024-05-29 15:06:37 +0800

    array

--- src/main.rs
+++ src/main.rs
@@ -2457,25 +2457,124 @@
 // }
 
 // 数组越界访问
-use std::io;
+// use std::io;
+// fn main() {
+//   let a = [1,2,3,4,5];
+//   println!("Please enter an array index");
+//   let mut index = String::new();
+//   // 读取控制台
+//   io::stdin()
+//     .read_line(&mut index)
+//     .expect("Fail to read line");
+
+//   let index: usize = index
+//       .trim()
+//       .parse()
+//       .expect("Index entered was not a number");
+
+//   let element =a[index];
+//   println!(
+//     "The value of the element at index {} is {}",
+//     index, element
+//   );
+// }
+
+// fn main() {
+//   let array: [String; 8] = std::array::from_fn(|_i| String::from("rust is good"));
+//   println!("{:#?}", array);
+// }
+
+// 数组切片
+// fn main() {
+//   let a: [i32; 5] = [1,2,3,4,5];
+//   let slice: &[i32] = &a[1..3];
+//   assert_eq!(slice, &[2,3]);
+// }
+
+// fn main() {
+//   let one = [1,2,3];
+
+//   let two: [u8; 3] = [1,2,3];
+//   let blank1 = [0;3];
+//   let blank2: [u8; 3] = [0;3];
+
+//   let arrays: [[u8; 3]; 4] = [one, two, blank1,blank2];
+//   for a in &arrays {
+//     println!("{:?}", a);
+
+//     for n in a.iter() {
+//       print!("\t{} + 10 = {}", n, n+10);
+//     }
+//     let mut sum = 0;
+//     for i in 0..a.len() {
+//       sum+=a[i];
+//     }
+//     println!("\t({:?} = {})", a, sum);
+//   }
+
+// }
+
+// practice
+
+// 1
+// fn main() {
+//   // 使用合适的类型填空
+//   let arr: [i32; 5] = [1, 2, 3, 4, 5];
+
+//   // 修改以下代码，让它顺利运行
+//   assert!(arr.len() == 5);
+// }
+
+// 2
+
+// fn main() {
+//   // 很多时候，我们可以忽略数组的部分类型，也可以忽略全部类型，让编译器帮助我们推导
+//   let arr0 = [1, 2, 3];
+//   let arr: [char; 3] = ['a', 'b', 'c'];
+  
+//   // 填空
+//   // 数组分配在栈上， `std::mem::size_of_val` 函数会返回整个数组占用的内存空间
+//   // 数组中的每个 char 元素占用 4 字节的内存空间，因为在 Rust 中， char 是 Unicode 字符
+//   assert!(std::mem::size_of_val(&arr) == 12);
+// }
+
+//3
+ 
+// fn main() {
+//   // 填空
+//   let list: [i32; 100] = [1;100] ;
+
+//   assert!(list[0] == 1);
+//   assert!(list.len() == 100);
+// }
+
+// 4
+
+// fn main() {
+//   // 修复错误
+//   let _arr = [1, 2, 3];
+// }
+
+// 5
+
+// fn main() {
+//   let arr = ['a', 'b', 'c'];
+  
+//   let ele = arr[0]; // 只修改此行来让代码工作
+
+//   assert!(ele == 'a');
+// }
+
+// 6
+
+// 修复代码中的错误
 fn main() {
-  let a = [1,2,3,4,5];
-  println!("Please enter an array index");
-  let mut index = String::new();
-  // 读取控制台
-  io::stdin()
-    .read_line(&mut index)
-    .expect("Fail to read line");
+  let names = [String::from("Sunfei"), "Sunface".to_string()];
+  
+  // `get` 返回 `Option<T>` 类型，因此它的使用非常安全
+  let name0 = names.get(0).unwrap();
+  println!("{}",name0);
 
-  let index: usize = index
-      .trim()
-      .parse()
-      .expect("Index entered was not a number");
-
-  let element =a[index];
-  println!(
-    "The value of the element at index {} is {}",
-    index, element
-  );
+  // 但是下标索引就存在越界的风险了
+  let _name1 = &names[0];
 }
-
