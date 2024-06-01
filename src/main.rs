7751c913abd7715662acdd33e2f7a7ddfb4aa89c
xoy <2088437179@qq.com>
2024-06-01 22:20:20 +0800

    match

--- src/main.rs
+++ src/main.rs
@@ -2860,3 +2860,156 @@
 
 //   assert!(count == 30)
 // }
+
+// 模式匹配
+// enum Direction {
+//     East,
+//     West,
+//     North,
+//     South,
+// }
+
+// fn main() {
+//   let dire = Direction::South;
+//   match dire {
+//       Direction::East => println!("East!"),
+//       Direction::North | Direction::South => {
+//         println!("South or North");
+//       },
+//       _=> println!("West"),
+//   };
+// }
+
+// enum Coin {
+//     Penny,
+//     Nickel,
+//     Dime,
+//     Quarter,
+// }
+
+// fn value_in_cents(coin: Coin) -> u8 {
+//   match coin {
+//       Coin::Penny=> {
+//         println!("Lucky penny!");
+//         1
+//       },
+//       Coin::Nickel=>5,
+//       Coin::Dime=>10,
+//       Coin::Quarter=>25,
+//   }
+// }
+
+// enum IpAddr {
+//     Ipv4,
+//     Ipv6,
+// }
+// fn main() {
+//   let ip1 = IpAddr::Ipv6;
+//   let ip_str = match ip1 {
+//       IpAddr::Ipv4=>"127.0.0.1",
+//       _=>"::1",
+//   };
+//   println!("{}",ip_str);
+// }
+
+// #[derive(Debug)]
+// enum UsState {
+//     Alabama,
+//     Alaska,
+// }
+// enum Coin {
+//     Penny,
+//     Nickel,
+//     Dime,
+//     Quarter(UsState),
+// }
+// fn value_in_cents(coin: Coin) -> u8 {
+//     match coin {
+//         Coin::Penny =>1,
+//         Coin::Nickel =>5,
+//         Coin::Dime =>10,
+//         Coin::Quarter(state) => {
+//           println!("State quarter from {:?}", state);
+//           25
+//         },
+//     }
+// }
+
+// fn main() {
+//   let coin = Coin::Quarter(UsState::Alaska);
+//   let result = value_in_cents(coin);
+//   println!("{}",result);
+// }
+
+// enum Action {
+//     Say(String),
+//     MoveTo(i32,i32),
+//     ChangeColorRGB(u16,u16,u16),
+// }
+
+// fn main() {
+//   let actions = [
+//     Action::Say("hello rust".to_string()),
+//     Action::MoveTo(1, 2),
+//     Action::ChangeColorRGB(255, 255, 0),
+//   ];
+//   for action in actions {
+//     match action {
+//         Action::Say(s) => {
+//           println!("{}",s);
+//         },
+//         Action::MoveTo(a, b) => {
+//           println!("point from (0,0) move to ({},{})",a,b);
+//         },
+//         Action::ChangeColorRGB(r, g, _) => {
+//           println!("change color into '(r:{}, g:{}, b:0)', 'b' has been ignored",
+//                 r,g);
+//         }
+//     }
+//   }
+// }
+
+// 穷尽匹配
+// enum Direction {
+//     East,
+//     West,
+//     North,
+//     South,
+// }
+
+// fn main() {
+//   let dire = Direction::South;
+//   match dire {
+//       Direction::East => println!("East"),
+//       Direction::North | Direction::West => {
+//         println!("North or West");
+//       },
+//   };
+// }
+
+// 通配符
+// fn main() {
+//   let some_u8_value = 0u8;
+//   match some_u8_value {
+//       1 => println!("one"),
+//       3 => println!("three"),
+//       5 => println!("five"),
+//       7 => println!("seven"),
+//       _ => (),
+//   }
+// }
+// 用一个变量来承载其他情况
+#[derive(Debug)]
+enum Direction {
+    East,
+    West,
+    North,
+    South,
+}
+fn main() {
+  let dire = Direction::South;
+  match dire {
+      Direction::East => println!("East"),
+      other => println!("{:?}", other),
+  };
+}
