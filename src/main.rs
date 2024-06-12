494911575541ee4d328bc67cb90df3c795adcbe2
xoy <2088437179@qq.com>
2024-06-12 11:25:10 +0800

    match

--- src/main.rs
+++ src/main.rs
@@ -3275,11 +3275,224 @@
 
 // }
 
-use std::vec;
+// use std::vec;
 
-fn main() {
-    let v = vec!['a', 'b','c'];
-    for (index, value) in v.iter().enumerate() {
-        println!("{} is at index {}", value, index);
-    }
-}
+// fn main() {
+//     let v = vec!['a', 'b','c'];
+//     for (index, value) in v.iter().enumerate() {
+//         println!("{} is at index {}", value, index);
+//     }
+// }
+
+// 全模式列表
+
+// 匹配字面值
+// fn main() {
+//     let x = 1;
+
+//     match x {
+//         1 => println!("one"),
+//         2 => println!("two"),
+//         3 => println!("three"),
+//         _ => println!("anything"),
+//     }
+// }
+
+// 匹配命名变量
+// fn main() {
+//     let x = Some(5);
+//     let y = 10;
+
+//     match x {
+//         Some(50) => println!("Got 50"),
+//         Some(y) => println!("Mached, y= {:?}",y),
+//         _ => println!("Default case, x= {:?}",x),
+//     }
+//     println!("at the end: x= {:?}, y = {:?}", x,y);
+// }
+
+// 单分支多模式
+// fn main() {
+//     let x = 1;
+
+//     match x {
+//         1 | 2 => println!("one or two"),
+//         3 => println!("three"),
+//         _ => println!("anything"),
+//     }
+// }
+
+// 通过序列 ..= 匹配值的范围
+// fn main() {
+//     // let x = 5;
+//     // match x {
+//     //     1..=5 => println!("one through five"),
+//     //     _ => println!("someThing else"),
+//     // }
+
+//     let x = 'c';
+//     match x {
+//         'a'..='j' => println!("early ASCII letter"),
+//         'k'..='z' => println!("late ASCII letter"),
+//         _ => println!("someThing else"),
+//     }
+// }
+
+// 解构并分解值
+// // 解构结构体
+// struct Point {
+//     x:i32,
+//     y:i32,
+// }
+// fn main() {
+//     let p = Point{x:0, y:7};
+//     // let Point{x:a,y:b} = p;
+//     // assert_eq!(0,a);
+//     // assert_eq!(7,b);
+
+//     let Point{x,y} = p;
+//     assert_eq!(0,x);
+//     assert_eq!(7,y);
+//  }
+
+// fn main() {
+//     let p = Point{x:0,y:7};
+
+//     match p {
+//         Point { x, y :0} =>println!("On the x axis ar {}", x),
+//         Point { x: 0, y} =>println!("On the y axis ar {}", y),
+//         Point { x, y } =>println!("On neither axis ar ({}, {})", x, y),
+//     }
+// }
+
+// 解构枚举
+// enum Message {
+//     Quit,
+//     Move {x:i32, y:i32},
+//     Write(String),
+//     ChangeColor(i32,i32,i32),
+// }
+// fn main() {
+//  let msg = Message::ChangeColor(0, 160, 255);
+
+//  match msg {
+//      Message::Quit => {
+//         println!("The Quit variant has no data to destructure.");
+//      },
+//      Message::Move { x, y } => {
+//         println!(
+//             "Move in the x direction {} and in the y direction {}",
+//             x,
+//             y
+//         );
+//      }
+//      Message::Write(text) => println!("Text message: {}",text),
+//      Message::ChangeColor(r, g, b) => {
+//         println!(
+//             "Change the color to red {}, green {}, and blue {}",
+//             r,
+//             g,
+//             b
+//         )
+//      }
+//  }
+// }
+// 解构嵌套的结构体和枚举
+// enum Color {
+//     Rgb(i32,i32,i32),
+//     Hsv(i32,i32,i32),
+// }
+// enum Message {
+//     Quit,
+//     Move {x:i32, y:i32},
+//     Write(String),
+//     ChangeColor(Color),
+// }
+// fn main() {
+//     let msg = Message::ChangeColor(Color::Hsv((0), (160), (255)));
+
+//     match msg {
+//         Message::ChangeColor(Color::Rgb(r, g, b)) => {
+//             println!(
+//                 "Change the color to red {}, green {}, and blue {}",
+//                 r,
+//                 g,
+//                 b
+//             )
+//         },
+//         Message::ChangeColor(Color::Hsv(h, s, v)) => {
+//             println!(
+//                 "Change the color to hue {}, saturation {}, and value {}",
+//                 h,
+//                 s,
+//                 v
+//             )
+//         },
+//         _ => {}
+//     }
+// }
+// 解构结构体和元组
+// struct Point {
+//     x: i32,
+//     y: i32,
+// }
+// fn main() {
+//     let ((feet, inches), Point {x,y}) = ((3,10), Point{x:3,y:-10});
+
+// }
+
+// 解构数组
+// fn main() {
+//     // 定长数组
+//     // let arr: [u16;2] = [114,514];
+//     // let [x,y] = arr;
+
+//     // assert_eq!(x,114);
+//     // assert_eq!(y,514);
+
+//     // 不定长数组
+//     let arr: &[u16] = &[114,514];
+//     if let [x, ..] = arr {
+//         assert_eq!(x, &114);
+//     }
+//     if let [.., y] = arr {
+//         assert_eq!(y, &514);
+//     }
+//     let arr: &[u16] = &[];
+
+//     assert!(matches!(arr, [..]));
+//     assert!(!matches!(arr, [x, ..]));
+// }
+
+// 忽略模式中的值
+// fn foo(_: i32, y: i32) {
+//     println!("This code only uses the y parameter: {}", y);
+// }
+// fn main() {
+//     foo(3, 4);
+// }
+// 使用嵌套的 _ 忽略部分值
+// fn main() {
+//     let mut setting_value = Some(5);
+//     let mut new_setting_value = Some(10);
+
+//     match (setting_value, new_setting_value) {
+//         (Some(_), Some(_)) => {
+//             println!("Can't overwrite an existing customized value");
+//         },
+//         _ => {
+//             setting_value = new_setting_value;
+//         }
+//     }
+//     println!("setting is {:?}", setting_value);
+// }
+
+// fn main() {
+//     let number = (2,4,8,16,32);
+
+//     match number {
+//         (first, _, third, _, fifth) => {
+//             println!("Some numbers: {}, {}, {}", first, third, fifth)
+//         },
+//     }
+// }
