c128c9496c3e8e497704bab19256aee49946fb5d
xoy <2088437179@qq.com>
2024-06-15 17:27:38 +0800

    Method impl

--- src/main.rs
+++ src/main.rs
@@ -3719,11 +3719,61 @@
 
 // 修复错误，尽量少地修改代码
 // 不要移除任何代码行
-fn main() {
-  let mut v = String::from("hello,");
-  let r = &mut v;
+// fn main() {
+//   let mut v = String::from("hello,");
+//   let r = &mut v;
 
-  match r {
-     &mut value => value.push_str(" world!") 
-  }
+//   match r {
+//     // &mut value => value.push_str(" world!") 
+//     value => value.push_str(" world!") 
+//   }
+// }
+
+
+// 方法
+// 方法定义
+// struct Circle {
+//   x: f64,
+//   y: f64,
+//   radius: f64,
+// }
+
+// impl Circle {
+//     // new是Circle的关联函数，因为它的第一个参数不是self，且new并不是关键字
+//     // 这种方法往往用于初始化当前结构体的实例
+
+//     fn new(x: f64, y: f64, radius: f64) -> Circle{
+//         Circle {
+//           x: x,
+//           y: y,
+//           radius: radius,
+//         }
+//     }
+//      // Circle的方法，&self表示借用当前的Circle结构体
+//     fn area(&self) -> f64 {
+//       std::f64::consts::PI * (self.radius * self.radius)
+//     }
+// }
+
+#[derive(Debug)]
+struct Rectangle {
+  width: u32,
+  height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32{
+      self.width * self.height
+    }
+}
+
+fn main() {
+  let rect1 = Rectangle {
+    width: 30,
+    height: 50,
+  };
+
+  println!(
+    "The area of the rectangle is {} square pixels.",
+    rect1.area())
 }
