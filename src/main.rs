5e3768930fbbd5b752b29a1d1003c9baece1651a
xoy <2088437179@qq.com>
2024-06-24 21:48:56 +0800

    method

--- src/main.rs
+++ src/main.rs
@@ -3798,20 +3798,186 @@
 
 // }
 // 实现 getter 访问器
-pub struct Rectangle {
-  width: u32,
-  height: u32,
+// pub struct Rectangle {
+//   width: u32,
+//   height: u32,
+// }
+
+// impl Rectangle {
+//   pub fn new(width: u32, height: u32) -> Self{
+//     Rectangle { width, height}
+//   }
+//   pub fn width(&self) ->u32 {
+//     return self.width;
+//   }
+// }
+// fn main() {
+//   let rect1 = Rectangle::new(30, 50);
+//   println!("{}", rect1.width())
+// }
+
+// 带有多个参数的方法
+// impl Rectangle {
+//     fn area(&self) -> u32 {
+//       self.width*self.height
+//     }
+//     fn can_hold(&self, other: &Rectangle) -> bool {
+//       self.width > other.width && self.height > other.height
+//     }
+// }
+// fn main() {
+//   let rect1 = Rectangle{width:30, height:50};
+//   let rect2 = Rectangle{width:10, height:40};
+//   let rect3 = Rectangle{width:60, height:45};
+
+//   println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+//   println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+// }
+
+// 关联函数
+// impl Rectangle {
+//     fn new(w: u32, h: u32) -> Rectangle {
+//       Rectangle{width:w, height:h}
+//     }
+// }
+
+// fn main() {
+//   let sq = Rectangle::new(3, 2);
+// }
+
+// 多个 impl 定义
+// impl Rectangle {
+//     fn area(&self) -> u32 {
+//       self.width*self.height
+//     }
+// }
+// impl Rectangle {
+//   fn can_hold(&self, other: &Rectangle) -> bool {
+//       self.width > other.width && self.height > other.height
+//   }
+// }
+
+// 为枚举实现方法
+// #![allow(unused)]
+// enum Message {
+//   Quit,
+//   Move {x: i32, y:i32},
+//   Write(String),
+//   ChangeColor(i32,i32,i32),
+// }
+
+// impl Message {
+//   fn call(&self) {
+//       // 在这里定义方法体
+//   }
+// }
+
+// fn main() {
+//   let m = Message::Write(String::from("hello"));
+//   m.call();
+// }
+// practice 
+struct Point {
+  x: f64,
+  y: f64,
+}
+
+// `Point` 的关联函数都放在下面的 `impl` 语句块中
+impl Point {
+  // 关联函数的使用方法跟构造器非常类似
+  fn origin() -> Point {
+      Point { x: 0.0, y: 0.0 }
+  }
+
+  // 另外一个关联函数，有两个参数
+  fn new(x: f64, y: f64) -> Point {
+      Point { x: x, y: y }
+  }
+}
+
+struct Rectangle {
+  p1: Point,
+  p2: Point,
 }
 
 impl Rectangle {
-  pub fn new(width: u32, height: u32) -> Self{
-    Rectangle { width, height}
+  // 这是一个方法
+  // `&self` 是 `self: &Self` 的语法糖
+  // `Self` 是当前调用对象的类型，对于本例来说 `Self` = `Rectangle`
+  fn area(&self) -> f64 {
+      // 使用点操作符可以访问 `self` 中的结构体字段
+      let Point { x: x1, y: y1 } = self.p1;
+      let Point { x: x2, y: y2 } = self.p2;
+
+
+      // `abs` 是一个 `f64` 类型的方法，会返回调用者的绝对值
+      ((x1 - x2) * (y1 - y2)).abs()
   }
-  pub fn width(&self) ->u32 {
-    return self.width;
+
+  fn perimeter(&self) -> f64 {
+      let Point { x: x1, y: y1 } = self.p1;
+      let Point { x: x2, y: y2 } = self.p2;
+
+      2.0 * ((x1 - x2).abs() + (y1 - y2).abs())
+  }
+
+  // 该方法要求调用者是可变的，`&mut self` 是 `self: &mut Self` 的语法糖
+  fn translate(&mut self, x: f64, y: f64) {
+      self.p1.x += x;
+      self.p2.x += x;
+
+      self.p1.y += y;
+      self.p2.y += y;
   }
 }
+
+// `Pair` 持有两个分配在堆上的整数
+struct Pair(Box<i32>, Box<i32>);
+
+impl Pair {
+  // 该方法会拿走调用者的所有权
+  // `self` 是 `self: Self` 的语法糖
+  fn destroy(self) {
+      let Pair(first, second) = self;
+
+      println!("Destroying Pair({}, {})", first, second);
+
+      // `first` 和 `second` 在这里超出作用域并被释放
+  }
+}
+
 fn main() {
-  let rect1 = Rectangle::new(30, 50);
-  println!("{}", rect1.width())
+  let rectangle = Rectangle {
+      // 关联函数的调用不是通过点操作符，而是使用 `::`
+      p1: Point::origin(),
+      p2: Point::new(3.0, 4.0),
+  };
+
+  // 方法才是通过点操作符调用
+  // 注意，这里的方法需要的是 `&self` 但是我们并没有使用 `(&rectangle).perimeter()` 来调用，原因在于：
+  // 编译器会帮我们自动取引用
+  //  `rectangle.perimeter()` === `Rectangle::perimeter(&rectangle)`
+  println!("Rectangle perimeter: {}", rectangle.perimeter());
+  println!("Rectangle area: {}", rectangle.area());
+
+  let mut square = Rectangle {
+      p1: Point::origin(),
+      p2: Point::new(1.0, 1.0),
+  };
+
+
+  // 错误！`rectangle` 是不可变的，但是这个方法要求一个可变的对象
+  //rectangle.translate(1.0, 0.0);
+  // TODO ^ 试着反注释此行，看看会发生什么
+
+  // 可以！可变对象可以调用可变的方法
+  square.translate(1.0, 1.0);
+
+  let pair = Pair(Box::new(1), Box::new(2));
+
+  pair.destroy();
+
+  // Error! 上一个 `destroy` 调用拿走了 `pair` 的所有权
+  //pair.destroy();
+  // TODO ^ 试着反注释此行
 }
