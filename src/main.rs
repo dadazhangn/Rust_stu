85579fa047f6a1332aeb0deac59fc82785a14382
xoy <2088437179@qq.com>
2024-07-03 20:46:48 +0800

    generic

--- src/main.rs
+++ src/main.rs
@@ -4305,27 +4305,39 @@
 // 1
 
 // 填空
-struct A;          // 具体的类型 `A`.
-struct S(A);       // 具体的类型 `S`.
-struct SGen<T>(T); // 泛型 `SGen`.
+// struct A;          // 具体的类型 `A`.
+// struct S(A);       // 具体的类型 `S`.
+// struct SGen<T>(T); // 泛型 `SGen`.
 
-fn reg_fn(_s: S) {}
+// fn reg_fn(_s: S) {}
 
-fn gen_spec_t(_s: SGen<A>) {}
+// fn gen_spec_t(_s: SGen<A>) {}
 
-fn gen_spec_i32(_s: SGen<i32>) {}
+// fn gen_spec_i32(_s: SGen<i32>) {}
 
-fn generic<T>(_s: SGen<T>) {}
+// fn generic<T>(_s: SGen<T>) {}
+
+// fn main() {
+//     // 使用非泛型函数
+//     reg_fn(S(A));          // 具体的类型
+//     gen_spec_t(SGen((A)));   // 隐式地指定类型参数  `A`.
+//     gen_spec_i32(SGen(2)); // 隐式地指定类型参数`i32`.
+
+//     // 显式地指定类型参数 `char`
+//     generic::<char>(SGen('s'));
+
+//     // 隐式地指定类型参数 `char`.
+//     generic(SGen('a'));
+// }
+
+
+// 实现下面的泛型函数 sum
+fn sum<T:std::ops::Add<Output = T>>(x: T, y:T) -> T{
+    x+y
+}
 
 fn main() {
-    // 使用非泛型函数
-    reg_fn(__);          // 具体的类型
-    gen_spec_t(__);   // 隐式地指定类型参数  `A`.
-    gen_spec_i32(__); // 隐式地指定类型参数`i32`.
-
-    // 显式地指定类型参数 `char`
-    generic::<char>(__);
-
-    // 隐式地指定类型参数 `char`.
-    generic(__);
+    assert_eq!(5, sum(2i8, 3i8));
+    assert_eq!(50, sum(20, 30));
+    assert_eq!(2.46, sum(1.23, 1.23));
 }
