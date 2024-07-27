1d83ed4d6a7b96b961da6ae575555a3fcbda3432
xoy <2088437179@qq.com>
2024-07-27 20:17:25 +0800

    Hash_Map

--- src/main.rs
+++ src/main.rs
@@ -6587,4 +6587,36 @@
 //     }
 // }
 
+// use std::collections::HashMap;
 
+// // HashMap
+// fn main() {
+//     // 创建一个HashMap，用于存储宝石种类和对应的数量
+//     let mut my_gems = HashMap::new();
+//     // 将宝石类型和对应的数量写入表中
+//     my_gems.insert("红宝石", 1);
+//     my_gems.insert("蓝宝石", 1);
+//     my_gems.insert("河边捡的破石头", 1);
+// }
+
+// use std::collections::HashMap;
+
+
+// // Vec<(String, u32)> 中的数据写入到 HashMap<String, u32>
+// fn main() {
+//     let team_list = vec![
+//         ("中国队".to_string(), 100),
+//         ("美国队".to_string(), 10),
+//         ("日本队".to_string(), 50),
+//     ];
+
+//     // let mut team_hashMap = HashMap::new();
+//     // for team in team_list {
+//     //     team_hashMap.insert(team.0, team.1);
+//     // }
+//     // println!("{:?}", team_hashMap);
+
+//     let team_hashMap: HashMap<_, _> = team_list.into_iter().collect();
+//     println!("{:?}", team_hashMap);
+
+// }
