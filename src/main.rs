bea348900cfeb3dffd503940b5aec99ed75881f2
xoy <2088437179@qq.com>
2024-07-30 21:29:45 +0800

    panic

--- src/main.rs
+++ src/main.rs
@@ -7143,24 +7143,232 @@
 
 // 4
 /* 使用三种方法修复下面的错误  */
-fn invalid_output<'a>(x: &'a String ) -> &'a String { 
-    &String::from("foo") 
-}
+// fn invalid_output<'a>(x: &'a String ) -> &'a String { 
+//     &String::from("foo") 
+// }
 
-fn main() {
-}
+// fn main() {
+// }
 
-/* 使用三种方法修复下面的错误  */
-// fn invalid_output<'a>() -> String { 
+// fn invalid_output<'a>() -> String {  
 //     String::from("foo") 
 // }
 
 // fn main() {
 // }
 
-fn invalid_output>() -> &'static String { 
-    &String::from("foo") 
-}
+// fn invalid_output() -> &'static str { 
+//     "foo" 
+// }
 
+// fn main() {
+// }
+
+// fn invalid_output<'a>(x: &'a String ) -> &'a String { 
+//     x
+// }
+
+// fn main() {
+// }
+
+// 5
+// `print_refs` 有两个引用参数，它们的生命周期 `'a` 和 `'b` 至少得跟函数活得一样久
+// fn print_refs<'a, 'b>(x: &'a i32, y: &'b i32) {
+//     println!("x is {} and y is {}", x, y);
+// }
+
+// /* 让下面的代码工作 */
+// fn failed_borrow<'a>() {
+//     let _x = 12;
+
+//     // ERROR: `_x` 活得不够久does not live long enough
+//     let y: & i32 = &_x;
+
+//     // 在函数内使用 `'a` 将会报错，原因是 `&_x` 的生命周期显然比 `'a` 要小
+//     // 你不能将一个小的生命周期强转成大的
+// }
+
+// fn main() {
+//     let (four, nine) = (4, 9);
+    
+
+//     print_refs(&four, &nine);
+//     // 这里，four 和 nice 的生命周期必须要比函数 print_refs 长
+    
+//     failed_borrow();
+//     // `failed_borrow`  没有传入任何引用去限制生命周期 `'a`，因此，此时的 `'a` 生命周期是没有任何限制的，它默认是 `'static`
+// }
+
+// 6
+/* 增加合适的生命周期标准，让代码工作 */
+
+// `i32` 的引用必须比 `Borrowed` 活得更久
+// #[derive(Debug)]
+// struct Borrowed<'a>(&'a i32);
+
+// // 类似的，下面两个引用也必须比结构体 `NamedBorrowed` 活得更久
+// #[derive(Debug)]
+// struct NamedBorrowed<'a> {
+//     x: &'a i32,
+//     y: &'a i32,
+// }
+
+// #[derive(Debug)]
+// enum Either<'a> {
+//     Num(i32),
+//     Ref(&'a i32),
+// }
+
+// fn main() {
+//     let x = 18;
+//     let y = 15;
+
+//     let single = Borrowed(&x);
+//     let double = NamedBorrowed { x: &x, y: &y };
+//     let reference = Either::Ref(&x);
+//     let number    = Either::Num(y);
+
+//     println!("x is borrowed in {:?}", single);
+//     println!("x and y are borrowed in {:?}", double);
+//     println!("x is borrowed in {:?}", reference);
+//     println!("y is *not* borrowed in {:?}", number);
+// }
+
+// 7 --
+/* 让代码工作 */
+
+// #[derive(Debug)]
+// struct NoCopyType {}
+
+// #[derive(Debug)]
+// struct Example<'a, 'b> {
+//     a: &'a u32,
+//     b: &'b NoCopyType
+// }
+
+// fn main()
+// { 
+//   let var_a = 35;
+//   let example: Example;
+  
+  
+//     let var_b = NoCopyType {};
+    
+//     /* 修复错误 */
+//     example = Example { a: &var_a, b: &var_b };
+  
+  
+//   println!("(Success!) {:?}", example);
+// }
+
+// 8
+
+// #[derive(Debug)]
+// struct NoCopyType {}
+
+// #[derive(Debug)]
+// #[allow(dead_code)]
+// struct Example<'a, 'b> {
+//     a: &'a u32,
+//     b: &'b NoCopyType
+// }
+
+// /* 修复函数的签名 */
+// fn fix_me<'b>(foo: &Example<'_, 'b>) -> &'b NoCopyType
+// { foo.b }
+
+// fn main()
+// {
+//     let no_copy = NoCopyType {};
+//     let example = Example { a: &1, b: &no_copy };
+//     fix_me(&example);
+//     println!("Success!")
+// }
+
+// struct Owner(i32);
+
+// impl Owner {
+//     fn add_one<'a>(&'a mut self) { self.0 += 1; }
+//     fn print<'a>(&'a self) {
+//         println!("`print`: {}", self.0);
+//     }
+// }
+
+// fn main() {
+//     let mut owner = Owner(18);
+
+//     owner.add_one();
+//     owner.print();
+// }
+
+// 9
+/* 添加合适的生命周期让下面代码工作 */
+// struct ImportantExcerpt<'a> {
+//     part: &'a str,
+// }
+
+// impl<'a> ImportantExcerpt<'a> {
+//     fn level(&'a self) ->  i32 {
+//         3
+//     }
+// }
+
+// fn main() {}
+
+// 10
+
+/* 移除所有可以消除的生命周期标注 */
+
+// fn nput(x: &i32) {
+//     println!("`annotated_input`: {}", x);
+// }
+
+// fn pass(x: &i32) -> & i32 { x }
+
+// fn longest<'a, 'b>(x: &'a str, y: &'b str) -> &'a str {
+//     x
+// }
+
+// struct Owner(i32);
+
+// impl Owner {
+//     fn add_one(& mut self) { self.0 += 1; }
+//     fn print(&self) {
+//         println!("`print`: {}", self.0);
+//     }
+// }
+
+// struct Person<'a> {
+//     age: u8,
+//     name: &'a str,
+// }
+
+// enum Either<'a> {
+//     Num(i32),
+//     Ref(&'a i32),
+// }
+
+// fn main() {}
+
+
+// 返回值和错误处理
+
+// panic! 与不可恢复错误
+// 被动触发
+// fn main() {
+//     let v = vec![1,2,3,4];
+
+//     v[99];  //数组访问越界
+// }
+
+// 主动调用
+// fn main() {
+//     panic!("crash and burn");
+// }
+
+// backtrace 栈展开
 fn main() {
+    let v = vec![1, 2, 3];
+
+    v[99];
 }
