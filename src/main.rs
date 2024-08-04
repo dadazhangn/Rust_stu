50e794526429fb09fb6a103277305182491821af
xoy <2088437179@qq.com>
2024-08-04 18:10:17 +0800

    package

--- src/main.rs
+++ src/main.rs
@@ -7709,44 +7709,94 @@
 // }
 
 // 5
-use std::num::ParseIntError;
+// use std::num::ParseIntError;
 
-// 使用 Result 重写后，我们使用模式匹配的方式来处理，而无需使用 `unwrap`
-// 但是这种写法实在过于啰嗦..
-fn multiply(n1_str: &str, n2_str: &str) -> Result<i32, ParseIntError> {
-    match n1_str.parse::<i32>() {
-        Ok(n1)  => {
-            match n2_str.parse::<i32>() {
-                Ok(n2)  => {
-                    Ok(n1 * n2)
-                },
-                Err(e) => Err(e),
-            }
-        },
-        Err(e) => Err(e),
-    }
-}
+// // 使用 Result 重写后，我们使用模式匹配的方式来处理，而无需使用 `unwrap`
+// // 但是这种写法实在过于啰嗦..
+// fn multiply(n1_str: &str, n2_str: &str) -> Result<i32, ParseIntError> {
+//     match n1_str.parse::<i32>() {
+//         Ok(n1)  => {
+//             match n2_str.parse::<i32>() {
+//                 Ok(n2)  => {
+//                     Ok(n1 * n2)
+//                 },
+//                 Err(e) => Err(e),
+//             }
+//         },
+//         Err(e) => Err(e),
+//     }
+// }
 
-// 重写上面的 `multiply` ，让它尽量简洁
-// 提示：使用 `and_then` 和 `map`
-fn multiply1(n1_str: &str, n2_str: &str) -> Result<i32, ParseIntError> {
-    // 实现...
-}
+// // 重写上面的 `multiply` ，让它尽量简洁
+// // 提示：使用 `and_then` 和 `map`
+// fn multiply1(n1_str: &str, n2_str: &str) -> Result<i32, ParseIntError> {
+//     // 实现...
+//     n1_str.parse::<i32>().and_then(|n1| {
+//         n2_str.parse::<i32>().map(|n2| n1 * n2)
+//     })
+  
+    
+// }
 
-fn print(result: Result<i32, ParseIntError>) {
-    match result {
-        Ok(n)  => println!("n is {}", n),
-        Err(e) => println!("Error: {}", e),
-    }
-}
+// fn print(result: Result<i32, ParseIntError>) {
+//     match result {
+//         Ok(n)  => println!("n is {}", n),
+//         Err(e) => println!("Error: {}", e),
+//     }
+// }
 
-fn main() {
-    let twenty = multiply1("10", "2");
-    print(twenty);
+// fn main() {
+//     let twenty = multiply1("10", "2");
+//     print(twenty);
 
-    // 下面的调用会提供更有帮助的错误信息
-    let tt = multiply("t", "2");
-    print(tt);
+//     // 下面的调用会提供更有帮助的错误信息
+//     let tt = multiply("t", "2");
+//     print(tt);
 
-    println!("Success!")
-}
+//     println!("Success!")
+// }
+
+
+// use std::num::ParseIntError;
+
+// // 填空
+// type Res<T> = std::result::Result<T, ParseIntError>;
+
+// // 使用上面的别名来引用原来的 `Result` 类型
+// fn multiply(first_number_str: &str, second_number_str: &str) -> Res<i32> {
+//     first_number_str.parse::<i32>().and_then(|first_number| {
+//         second_number_str.parse::<i32>().map(|second_number| first_number * second_number)
+//     })
+// }
+
+// // 同样, 这里也使用了类型别名来简化代码
+// fn print(result: Res<i32>) {
+//     match result {
+//         Ok(n)  => println!("n is {}", n),
+//         Err(e) => println!("Error: {}", e),
+//     }
+// }
+
+// fn main() {
+//     print(multiply("10", "2"));
+//     print(multiply("t", "2"));
+
+//     println!("Success!")
+// }   
+
+
+
+// use std::num::ParseIntError;
+
+// fn main() -> Result<(), ParseIntError> {
+//     let number_str = "10";
+//     let number = match number_str.parse::<i32>() {
+//         Ok(number)  => number,
+//         Err(e) => return Err(e),
+//     };
+//     println!("{}", number);
+//     Ok(())
+// }
+
+
+// package
