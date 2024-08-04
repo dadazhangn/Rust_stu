262c8c46b2092ceb1bed9e2c5369011dabfbd923
xoy <2088437179@qq.com>
2024-08-04 21:15:53 +0800

    package

--- src/main.rs
+++ src/main.rs
@@ -7800,3 +7800,29 @@
 
 
 // package
+// use std::fmt;
+// use std::io;
+
+// fn function1() -> fmt::Result {
+//     // --snip--
+// }
+
+// fn function2() -> io::Result<()> {
+//     // --snip--
+// }
+
+
+// use std::fmt::Result;
+// use std::io::Result as IoResult;
+
+// fn function1() -> Result {
+//     // --snip--
+// }
+
+// fn function2() -> IoResult<()> {
+//     // --snip--
+// }
+
+
+
+
