18315c0b8353dfcf9b4a58f97dd075db7cd16f82
xoy <2088437179@qq.com>
2024-08-06 21:06:02 +0800

    guess

--- src/main.rs
+++ src/main.rs
@@ -7825,4 +7825,37 @@
 
 
 
+use std::{cmp::Ordering, io};
 
+use rand::Rng;
+// 猜数字
+fn main() {
+
+    let number = rand::thread_rng().gen_range(0..=100);
+    println!("secret number is: {number}");
+
+    loop {
+        let mut input = String::new();
+        // 获取输入
+        println!("please input your guess(0-100):>");
+        io::stdin().read_line(&mut input).expect("Faild to read line");
+
+        println!("your guess {input}");
+
+        let guess: i32= match input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(& number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("you win!");
+                break;
+        }
+            }
+    }
+
+
+}
