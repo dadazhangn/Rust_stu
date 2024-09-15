1d650f5443e6cd43f9ea45a19246c228478a5860
xoy <2088437179@qq.com>
2024-09-15 17:02:07 +0800

    iter

--- src/main.rs
+++ src/main.rs
@@ -8663,4 +8663,78 @@
 // fn shoes_in_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
 //     shoes.into_iter().filter(|s| s.size == shoe_size).collect()
 // }
-// 实现 Iterator 特征
+// 实现iterator特征
+// 创建一个计数器
+// struct Counter {
+//     count: u32,
+// }
+
+// impl  Counter{
+//     fn new() -> Counter {
+//         Counter {
+//             count: 0
+//         }
+//     }
+// }
+
+// impl Iterator for Counter {
+//     type Item = u32;
+
+//     fn next(&mut self) -> Option<Self::Item> {
+//         self.count += 1;
+//         if self.count < 6 {
+//             Some(self.count)
+//         } else {
+//             None
+//         }
+//     }
+// }
+
+
+// fn main() {
+//     // let mut counter = Counter::new();
+//     // println!("{:?}", counter.next());
+//     // println!("{:?}", counter.next());
+//     // println!("{:?}", counter.next());
+//     // println!("{:?}", counter.next());
+//     // println!("{:?}", counter.next());
+//     // println!("{:?}", counter.next());
+
+//     // 实现 Iterator 特征的其它方法
+
+//     let sum: u32 = Counter::new()
+//                         .zip(Counter::new().skip(1))
+//                         .map(|(a, b)| a * b)
+//                         .filter(|x| x % 3 == 0)
+//                         .sum();
+//     assert_eq!(18, sum);
+// }
+
+// enumerate
+// fn main() {
+//     let v = vec![1u64, 2,3,4,5,6];
+//     for (i, v) in v.iter().enumerate() {
+//         println!("第{}个值是{}", i,v);
+//     }
+// }
+// fn main() {
+//     let v = vec![1u64, 2,3,4,5,6];
+//     let val = v.iter().enumerate()
+//     .filter(|&(idx, _)| idx % 2 == 0)
+//     .map(|(_, val)| val)
+//     .fold(0u64, |sum, acm| sum + acm);
+// }
+// 迭代器的性能
+
+fn sum_for(x: &[f64]) -> f64 {
+    let mut result = 0f64;
+    for i in 0..x.len() {
+        result += x[i];
+    }
+    result
+}
+
+fn sum_iter(x: &[f64]) -> f64{
+    x.iter().sum::<f64>()
+}
+
