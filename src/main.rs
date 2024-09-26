9f6f0b1c908411959340fe3f1de9878ce886e787
xoy <2088437179@qq.com>
2024-09-26 16:31:41 +0800

    Arc

--- src/main.rs
+++ src/main.rs
@@ -8957,4 +8957,85 @@
 //     }
 // }
 
+// Rc<T>
+// use std::rc::Rc;
+// fn main() {
+//     let a = Rc::new(String::from("hello world"));
+//     let b = Rc::clone(&a);
 
+//     assert_eq!(2, Rc::strong_count(&a));
+//     assert_eq!(Rc::strong_count(&a), Rc::strong_count(&b));
+
+// }
+
+// use std::rc::Rc;
+// fn main() {
+//     let a = Rc::new(String::from("test ref counting"));
+//     println!("count after creating a = {}", Rc::strong_count(&a));
+//     let b = Rc::clone(&a);
+//     println!("count after creating b = {}", Rc::strong_count(&a));
+
+//     {
+//         let c = Rc::clone(&a);
+//         println!("count after creating c = {}", Rc::strong_count(&c));
+//     }
+//     println!("count after c gose of scope = {}", Rc::strong_count(&a));
+// }
+
+// use std::rc::Rc;
+
+// struct Owner {
+//     name: String,
+// }
+
+// struct Gadget {
+//     id:i32,
+//     owner: Rc<Owner>
+// }
+
+// fn main() {
+//     // 创建一个基于引用计数的Owner
+//     let gadget_owner = Rc::new(Owner{name: "Gadget Man".to_string()});
+
+//     // 创建两个不同的工具，他们属于同一主人
+//     let gadget1 = Gadget {
+//         id: 1,
+//         owner: Rc::clone(&gadget_owner),
+//     };
+
+//     let gadget2 = Gadget {
+//         id:2,
+//         owner: Rc::clone(&gadget_owner),
+//     };
+
+//     // 释放掉第一个Rc<Owner>
+//     drop(gadget_owner);
+//     //尽管在上面我们释放了 gadget_owner，但是依然可以在这里使用 owner 的信息
+//     // 原因是在 drop 之前，存在三个指向 Gadget Man 的智能指针引用，上面仅仅
+//     // drop 掉其中一个智能指针引用，而不是 drop 掉 owner 数据，外面还有两个
+//     // 引用指向底层的 owner 数据，引用计数尚未清零
+//     // 因此 owner 数据依然可以被使用
+//     println!("Gadget {} owned by {}", gadget1.id, gadget1.owner.name);
+//     println!("Gadget {} owned by {}", gadget2.id, gadget2.owner.name);
+
+//     // 在函数最后，`gadget1` 和 `gadget2` 也被释放，最终引用计数归零，随后底层
+//     // 数据也被清理释放
+
+// }
+
+// 多线程场景使用 Rc<T>
+// use std::rc::Rc;
+// use std::thread;
+
+// fn main() {
+//     let s = Rc::new(String::from("多线程漫游者"));
+
+//     for _ in 0..10 {
+//         let s = Rc::clone(&s);
+//         let handle = thread::spawn(move || {
+//             println!("{}", s)
+//         });
+//     }
+// }
+
+// Arc
