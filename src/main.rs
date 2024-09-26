7e7a545f14d7957e43f38395af91a8d824b296f8
xoy <2088437179@qq.com>
2024-09-26 17:04:14 +0800

    Cell

--- src/main.rs
+++ src/main.rs
@@ -9039,3 +9039,17 @@
 // }
 
 // Arc
+// use std::sync::Arc;
+// use std::thread;
+
+// fn main() {
+//     let s = Arc::new(String::from("多线程漫游者"));
+//     for _ in 1..10 {
+//         let s = Arc::clone(&s);
+//         let handle = thread::spawn(move || {
+//             println!("{}", s)
+//         }
+//     );
+//     }
+// }
+// Cell
