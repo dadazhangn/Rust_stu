8da0d4805582588316ca38255fc2d83a5df9dc53
xoy <2088437179@qq.com>
2024-09-27 14:08:46 +0800

    thread

--- src/main.rs
+++ src/main.rs
@@ -9053,3 +9053,116 @@
 //     }
 // }
 // Cell
+// use std::cell::Cell;
+// fn main() {
+//     let c = Cell::new("wwas");
+//     // &str实现了copy特征
+//     let one = c.get();
+//     c.set("qwer");
+//     let two = c.get();
+//     println!("{}, {}", one, two);
+// }
+
+// RefCell
+// use std::cell::RefCell;
+
+// fn main() {
+//     let s = RefCell::new(String::from("hello world"));
+//     let s1 = s.borrow();
+//     let s2 = s.borrow_mut();
+// }
+
+// use std::cell::Cell;
+// fn main() {
+//     let x = Cell::new(1);
+//     let y =  &x;
+//     let z = &x;
+//     x.set(2);
+//     y.set(3);
+//     z.set(4);
+//     println!("{}", x.get());
+// }
+
+// fn main() {
+//     let mut x = 1;
+//     let y = &mut x;
+//     let z = &mut x;
+//     x = 2;
+//     *y = 3;
+//     *z = 4;
+//     println!("{}", x);
+// }
+// 内部可变性
+
+// 定义在外部库中的特征
+// use std::cell::RefCell;
+// pub trait Messenger {
+//     fn send(&self, msg: String);
+// }
+
+// struct MsgQueue {
+//     msg_cache: RefCell<Vec<String>>,
+// }
+
+// impl Messenger for MsgQueue {
+//     fn send(&self, msg: String) {
+//         self.msg_cache.borrow_mut().push(msg);
+//     }
+// }
+
+// fn main() {
+//     let mq = MsgQueue {
+//         msg_cache: RefCell::new(Vec::new())
+//     };
+//     mq.send("hello".to_string());
+// }
+
+// Rc + RefCell 组合使用
+// use std::rc::Rc;
+// use std::cell::RefCell;
+
+// fn main() {
+//     let s = Rc::new(RefCell::new("我很善变，还有多个主人".to_string()));
+
+//     let s1 = s.clone();
+//     let s2 = s.clone();
+
+//     s2.borrow_mut().push_str(", oh year!");
+
+//     println!("{:?}\n{:?}\n{:?}", s,s1,s2);
+// }
+
+// fn is_even(i:i32) -> bool {
+//     i%2 ==0
+// }
+
+// fn retain_even(nums: &mut Vec<i32>) {
+//     let mut i = 0;
+//     for num in nums.iter().filter(|&num| is_even(*num)) {
+//         nums[i] = *num;
+//         i+=1;
+//     }
+//     nums.truncate(i);
+// }
+
+// 循环引用与自引用
+
+
+// 并发和并行
+// 创建线程
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {} for thr spawned thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        println!("hi number {} from the main thread", i);
+        thread::sleep(Duration::from_millis(1));
+    } 
+}
