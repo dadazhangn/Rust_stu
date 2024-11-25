ca966859c371b08e47458e875082fbafc4618ae8
xoy <2088437179@qq.com>
2024-11-25 22:09:58 +0800

    send

--- src/main.rs
+++ src/main.rs
@@ -9659,36 +9659,95 @@
 //     }// 写锁在此处被drop
 // }
 
-// 使用 Atomic 作为全局变量
-use std::ops::Sub;
-use std::sync::atomic::{AtomicU64, Ordering};
-use std::thread::{self, JoinHandle};
-use std::time::Instant;
+// // 使用 Atomic 作为全局变量
+// use std::ops::Sub;
+// use std::sync::atomic::{AtomicU64, Ordering};
+// use std::thread::{self, JoinHandle};
+// use std::time::Instant;
 
-const N_TIMES: u64 = 10000000;
-const N_THREADS: usize = 10;
+// const N_TIMES: u64 = 10000000;
+// const N_THREADS: usize = 10;
 
-static R: AtomicU64 = AtomicU64::new(0);
+// static R: AtomicU64 = AtomicU64::new(0);
 
-fn add_n_times(n: u64) -> JoinHandle<()> {
-    thread::spawn(move || {
-        for _ in 0..n {
-            R.fetch_add(1, Ordering::Relaxed);
-        }
-    })
-}
+// fn add_n_times(n: u64) -> JoinHandle<()> {
+//     thread::spawn(move || {
+//         for _ in 0..n {
+//             R.fetch_add(1, Ordering::Relaxed);
+//         }
+//     })
+// }
+
+// fn main() {
+//     let s = Instant::now();
+//     let mut threads = Vec::with_capacity(N_THREADS);
+
+//     for _ in 0..N_THREADS {
+//         threads.push(add_n_times(N_TIMES));
+//     }
+
+//     for thread in threads {
+//         thread.join().unwrap();
+//     }
+//     assert_eq!(N_TIMES * N_THREADS as u64, R.load(Ordering::Relaxed));
+//     println!("{:?}", Instant::now().sub(s));
+// }
+
+// 内存屏障的例子
+// use std::thread::{self, JoinHandle};
+// use std::sync::atomic::{Ordering, AtomicBool};
+
+// static mut DATA: u64 = 0;
+// static READY: AtomicBool = AtomicBool::new(false);
+
+// fn reset() {
+//     unsafe {
+//         DATA = 0;
+//     }
+//     READY.store(false, Ordering::Relaxed);
+// }
+
+// fn producter() -> JoinHandle<()> {
+//     thread::spawn(move || {
+//         unsafe {
+//             DATA = 100;
+//         }
+//         READY.store(true, Ordering::Release);
+//     }) 
+// }
+
+// fn consumer() -> JoinHandle<()>{
+//     thread::spawn(move || {
+//         while !READY.load(Ordering::Acquire) {
+            
+//         }
+//         assert_eq!(100, unsafe {DATA})
+//     })
+// }
+
+
+// fn main() {
+//     loop {
+//         reset();
+//         let t_producter = producter();
+//         let t_consumer = consumer();
+
+
+//         t_producter.join().unwrap();
+//         t_consumer.join().unwrap();
+//     }
+// }
+
+// 基于 Send 和 Sync 的线程安全
+use std::thread;
+use std:: rc::Rc;
+
+// use rand::thread_rng;
 
 fn main() {
-    let s = Instant::now();
-    let mut threads = Vec::with_capacity(N_THREADS);
-
-    for _ in 0..N_THREADS {
-        threads.push(add_n_times(N_TIMES));
-    }
-
-    for thread in threads {
-        thread.join().unwrap();
-    }
-    assert_eq!(N_TIMES * N_THREADS as u64, R.load(Ordering::Relaxed));
-    println!("{:?}", Instant::now().sub(s));
+    let v = Rc::new(5);
+    let t = thread::spawn(move || {
+        println!("{}", v);
+    });
+    t.join().unwrap();
 }
